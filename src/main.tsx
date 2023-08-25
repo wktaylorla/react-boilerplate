e51e4dc44c12c0197805ee860cc3845d94665b45
taylor-nurihaus <wktaylorla@gmail.com>
2023-08-25 15:29:30 +0900

    ✨ react-query

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,22 @@ import React from "react";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import { PersistGate } from "redux-persist/integration/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import App from "./App.tsx";
+import App from "./App";
 
-import store, { persistor } from "./store/configureStore.ts";
+import store, { persistor } from "./store/configureStore";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </PersistGate>
     </Provider>
   </React.StrictMode>
