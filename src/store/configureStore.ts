2b0d9ac2df3446fb3e31fee74e06a4e7b21cb294
taylor-nurihaus <wktaylorla@gmail.com>
2023-08-25 17:55:59 +0900

    🩹 ignore warning about persist actions

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,5 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { persistReducer, persistStore } from "redux-persist";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
 import rootReducer from "./rootReducer";
@@ -13,6 +22,13 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: true,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
