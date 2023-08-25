7b94afe13ef5865f7490cb67178c4f0760b75e98
taylor-nurihaus <wktaylorla@gmail.com>
2023-08-25 16:21:25 +0900

    🚚 make provider file

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,16 @@
 import React from "react";
-import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
-import { PersistGate } from "redux-persist/integration/react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import AppProvider from "./providers";
 
 import App from "./App";
 
-import store, { persistor } from "./store/configureStore";
 import "./index.css";
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
-      </PersistGate>
-    </Provider>
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>
 );
---
+++ src/providers/index.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import store, { persistor } from "../store/configureStore";
+
+const queryClient = new QueryClient();
+
+const AppProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </PersistGate>
+    </Provider>
+  );
+};
+
+export default AppProvider;
