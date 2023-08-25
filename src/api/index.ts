0387cf2026d64884c340571399afd8db07db3ba1
taylor-nurihaus <wktaylorla@gmail.com>
2023-08-25 16:00:40 +0900

    ✨ axios

---
+++ src/api/index.ts
@@ -0,0 +1,23 @@
+import instance from "./instance";
+
+interface Option {
+  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+  url: string;
+  data?: object;
+  "Content-Type"?:
+    | "application/json"
+    | "multipart/form-data"
+    | "application/x-www-form-urlencoded";
+}
+
+export const requestWithAuth = async (option: Option, token: string) => {
+  instance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+  const response = await instance(option);
+  return response.data;
+};
+
+export const requestWithoutAuth = async (option: Option) => {
+  const response = await instance(option);
+  return response.data;
+};
