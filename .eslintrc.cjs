825e8f796a2c893e446e549f75ddd6342a321a21
taylor-nurihaus <wktaylorla@gmail.com>
2023-08-28 16:19:25 +0900

    ✨ eslint + prettier + airbnb

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,39 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts', 'vite-env.d.ts'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
   },
-}
+  plugins: ['react-refresh', '@typescript-eslint'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': [
+      'error',
+      {
+        props: true,
+        ignorePropertyModificationsFor: ['state'],
+      },
+    ] /* exception for redux toolkit */,
+    'react/jsx-one-expression-per-line': 'off',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'object-curly-newline': 'off' /* conflict with prettier option (printWidth: 120) */,
+    'implicit-arrow-linebreak': 'off' /* conflict with prettier option (printWidth: 120) */,
+  },
+};
