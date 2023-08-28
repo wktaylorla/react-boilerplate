18279c76a4bfe0a52f815f77edb02597318083de
taylor-nurihaus <wktaylorla@gmail.com>
2023-08-28 13:34:40 +0900

    ✨ dotenv

---
+++ src/config/envConfig.ts
@@ -0,0 +1,7 @@
+const { VITE_API_URL } = import.meta.env;
+
+const envConfig = {
+  API_URL: VITE_API_URL,
+};
+
+export default envConfig;
---
+++ vite-env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  readonly VITE_API_URL: number;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
